5ffc24392733d7912422650a86085834b22618a4
ssstein <ssstein@rip.local>
2024-11-06 21:17:06 -0500

    first commit

---
+++ modules/darwin/casks.nix
@@ -0,0 +1,32 @@
+_:
+
+[
+  # Development Tools
+  "homebrew/cask/docker"
+  "visual-studio-code"
+  "utm"
+  "virtualbox"
+  "docker-machine-driver-vmware"
+  "wine-stable"
+  "zenith"
+
+  # Communication Tools
+  "discord"
+  "notion"
+  "slack"
+  "telegram"
+  "zoom"
+
+  # Utility Tools
+  "syncthing"
+
+  # Entertainment Tools
+  "vlc"
+
+  # Productivity Tools
+  "raycast"
+
+  # Browsers
+  "google-chrome"
+  "firefox@developer-edition"
+]
---
+++ modules/darwin/packages.nix
@@ -0,0 +1,11 @@
+{ pkgs }:
+
+with pkgs;
+let shared-packages = import ../shared/packages.nix { inherit pkgs; }; in
+shared-packages ++ [
+  dockutil
+  spacebar
+  shortcat
+  netsurf
+  
+]
